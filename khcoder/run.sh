2b24f0562c7161a21b018b1c6ea2f7911c3e6a18
王　詩博 <sibo_wang@ot.olympus.co.jp>
2019-09-13 18:02:20 +0900

    update khcoder Dockerfile

--- khcoder/run.sh
+++ khcoder/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d  -p 6080:80 -p 5900:5900 -e RESOLUTION=1280x720 -v /dev/shm:/dev/shm khcoder
+#docker run -d  -p 6080:80 -p 5900:5900 -e RESOLUTION=1280x720 -v /dev/shm:/dev/shm khcoder
+docker run --name khcoder \
+           -p 3389:3389 \
+           -p 2222:22 \
+           -v $(pwd)/createusers:/root/createusers.txt \
+           -v $(pwd)/home:/home \
+           -dit --restart unless-stopped \
+           khcoder
